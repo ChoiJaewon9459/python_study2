45ef931ed1e5620befc1b19baf4d60d59b22ffcf
ChoiJaewon9459 <dajeo9459@gmail.com>
2022-10-21 17:46:44 +0900

    표준 타입형

--- test01.py
+++ test01.py
@@ -1,3 +1,39 @@
+from decimal import Decimal
+import math as ma
+#print("안녕")
 
+num = 3 + 3
+num2  = 3- 3
+num3  = 3 * 3
+num4  = 3 / 3
 
-print("안녕")
+print(num)
+print(num2)
+print(num3)
+print(num4)
+
+num = 3 ** 3
+
+print(num)
+num = 2/3
+print(num)
+num = 1.1 + 0.1
+print(num)
+num = Decimal('1.1') + Decimal('0.1')
+print(num)
+
+name = "jaewon"
+age = 10.5
+print("내 이름은 "+name+"내 나이는"+str(age))
+print(f"내 이름은 {name}내 나이는{age}")
+#포맷은 재설정
+#
+print("내 이름은 %s 내 나이는 %d",(name,age))
+
+#list 에 [1,2,3,4] 
+# 0번쨰 변경할수 있음 이유 리스트는 가변이라서 
+
+chage_string = string.replace('가','하')
+
+print(id(string))
+print(id(chage_string))
---
+++ tt02.py
@@ -0,0 +1,34 @@
+# #data = int(input())
+
+
+# data = 1
+
+# if data == 1:
+#     print("같다")
+# elif data == 2:
+#     print("2다")
+# else:
+#     print("몰루?")
+
+# from xmlrpc.client import boolean
+
+
+
+# while True:
+#     print("가나다")
+#     for item in [1,2,3,4]:
+#         print(item)
+        
+def check_data():
+    print("가나다")        
+        
+check_data()
+check_data()
+check_data()
+check_data()
+check_data()
+check_data()
+check_data()
+check_data()
+
+            
